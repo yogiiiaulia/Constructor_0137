aa7b427e0e73257f4f104c45e20e17cd8f44270b
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-13 19:07:20 +0700

    mulai

---
+++ constructoroverloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+    private:
+    int nim;
+    string nama;
+    public :
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa::mahasiswa(){
+   
+}
+
+mahasiswa::mahasiswa(int iNim){
+    nim = iNim;
+}
+
+mahasiswa::mahasiswa(int iNama){
+    nama = iNama;
+}
